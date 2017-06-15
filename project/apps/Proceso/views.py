d3b0a0a7939337f56f454dec784a9ce2a46af464
David <davidaapariciobak@gmail.com>
2017-06-15 19:57:31 -0400

    Urls y views modificadas

--- project/apps/Proceso/views.py
+++ project/apps/Proceso/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-<<<<<<< HEAD:project/apps/prueba/views.py
+#<<<<<<< HEAD:project/apps/prueba/views.py
 def index(request):
     return render(request, 'login.html')
-=======
->>>>>>> 1af6289f378f9e59417fd04e4a15aaa8f39beaeb:project/apps/Proceso/views.py
+#=======
+#>>>>>>> 1af6289f378f9e59417fd04e4a15aaa8f39beaeb:project/apps/Proceso/views.py
