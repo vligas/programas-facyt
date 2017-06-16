c12166fcb90b923662e4e975376a466621831414
vligas <vligin2@gmail.com>
2017-06-16 12:43:15 -0400

    Cambios en css

--- project/config/settings.py
+++ project/config/settings.py
@@ -152,4 +152,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = reverse_lazy('login')
-LOGIN_REDIRECT_URL = reverse_lazy('index')
+LOGIN_REDIRECT_URL = reverse_lazy('home')
