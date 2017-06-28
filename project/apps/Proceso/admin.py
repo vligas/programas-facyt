e3fc1c0f533821fd8b6d587a00e142e2cb82e608
vligas <vligin2@gmail.com>
2017-06-27 20:03:21 -0400

    Añadido archivos en el admin y arreglado bug en template

--- project/apps/Proceso/admin.py
+++ project/apps/Proceso/admin.py
@@ -4,10 +4,18 @@ from . import models
 # Register your models here.
 @admin.register(models.Programas)
 class ProgramasModelAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'periodo_electivo', 'periodo_annio']
+    list_display = ['__str__', 'periodo_electivo', 'periodo_annio', 'file']
     search_fields = ['^codigo_materia', '^nombre', '^periodo_annio']
+
+    def file(self, obj):
+        if obj.archivo:
+            return '{}'.format(obj.archivo.documento.name)
+        else:
+            return 'None'
 
 @admin.register(models.Archivo)
 class ArchivoModelAdmin(admin.ModelAdmin):
     pass
+
+
 admin.site.register(models.Solicitud)
